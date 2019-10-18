8e5c7e2967a0f062ad10687ebf08e9fb4929af0b
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-18 21:40:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  include Findable::VariableMethods
+  extend Findable::VariableMethods
   attr_accessor :name
   attr_reader :songs
 
