18a71e76f07f9c08845c4bad9287a1d6c9b066d4
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-18 21:58:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,15 +4,16 @@ class Artist
   extend Memorable::ClassMethods
     extend Findable::ClassMethods
     include Paramable::InstanceMethods
+    include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def initialize
-    @@artists << self
+ def initialize
+    super
     @songs = []
- end
+  end
 
 
 
