22fd1201650175e461b311c5ebbb04d0e1fa6817
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-18 21:50:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,17 +8,17 @@ class Song
 
   @@songs = []
 
-  #def initialize
- #   @@songs << self
- # end
+  def initialize
+    @@songs << self
+  end
 
  # def self.find_by_name(name)
  #   @@songs.detect{|a| a.name == name}
  # end
 
- # def self.all
- #   @@songs
- # end
+  def self.all
+    @@songs
+  end
 
   def artist=(artist)
     @artist = artist
